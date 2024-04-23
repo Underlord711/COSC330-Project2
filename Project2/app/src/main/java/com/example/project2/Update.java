e7931bf1eb617fa24977459ad4af94192297c08b
Underlord711 <wchmar711@gmail.com>
2024-04-23 14:35:02 -0400

    Added functionality to the calendarview

--- Project2/app/src/main/java/com/example/project2/Update.java
+++ Project2/app/src/main/java/com/example/project2/Update.java
@@ -1,18 +1,28 @@
 package com.example.project2;
 
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import java.util.Date;
+
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-public class Update extends AppCompatActivity {
+import android.widget.CalendarView;
+
+import java.util.Locale;
+
+ public class Update extends AppCompatActivity {
+    CalendarView date;
     Button update;
 
     @Override
@@ -25,7 +35,21 @@ public class Update extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    update=findViewById(R.id.button3);
+
+        update = findViewById(R.id.button3);
+        date = findViewById(R.id.calendarView4);
+
+        // Set a listener to get the selected date
+        // Set a listener to get the selected date
+
+        date.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // Log the selected date when it changes
+                String selectedDate = formatDate(year, month, dayOfMonth);
+                Log.d("SelectedDate", "Date Selected: " + selectedDate);
+            }
+        });
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,4 +59,11 @@ public class Update extends AppCompatActivity {
             }
         });
     }
-}
+     private String formatDate(int year, int month, int dayOfMonth) {
+         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+         Calendar calendar = Calendar.getInstance();
+         calendar.set(year, month, dayOfMonth);
+         return sdf.format(calendar.getTime());
+     }
+ }
+
