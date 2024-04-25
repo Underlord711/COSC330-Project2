d171bd6c6595dfc7ee61f16a784493988702aa36
Underlord711 <wchmar711@gmail.com>
2024-04-25 14:00:49 -0400

    gradle changes

--- Project2/app/build.gradle.kts
+++ Project2/app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation 'com.jjoe64:graphview:4.2.2'
 
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
 }
