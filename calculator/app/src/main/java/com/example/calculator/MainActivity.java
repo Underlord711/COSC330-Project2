630af7ab37369d58b1e48bd495edcd34ded97a92
Rob Pellegrin <rob@localhost.localdomain>
2024-04-25 14:07:47 -0400

    Hi mom

---
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mNumberDisplay;
+    private SimpleExpression mExpression;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNumberDisplay = (TextView) findViewById(R.id.textView1);
+
+        mExpression = new SimpleExpression();
+    }
+
+    public void goAC(View view) {
+        mExpression.clearOperands();
+        mNumberDisplay.setText("0");
+    }
+
+    public void goOperand(View view) {
+        String val = (String) mNumberDisplay.getText();
+        String digit = (String) view.getContentDescription();
+        if (val.charAt(0) == '0')
+            mNumberDisplay.setText(digit);
+        else
+            mNumberDisplay.setText((String) mNumberDisplay.getText()
+                    + digit.charAt(0));
+    }
+
+    public void goOperator(View view) {
+        String operator = (String) view.getContentDescription();
+        try {
+            String val = (String) mNumberDisplay.getText();
+            mExpression.setOperand1((int) Integer.parseInt(val.toString()));
+        } catch (NumberFormatException e) {
+            mExpression.setOperand1(0);
+        }
+        mNumberDisplay.setText("0");
+        mExpression.setOperator(operator);
+    }
+
+
+    //WHEN THE = BUTTON IS CLICKED, COMPUTE AND DISPLAY THE VALUE
+    public void goCompute(View view) {
+        try {
+            String val = (String) mNumberDisplay.getText();
+            mExpression.setOperand2((int) Integer.parseInt(val.toString()));
+        } catch (NumberFormatException e) {
+            mExpression.setOperand2(0);
+        }
+        mNumberDisplay.setText(mExpression.getValue().toString());
+    }
+}
