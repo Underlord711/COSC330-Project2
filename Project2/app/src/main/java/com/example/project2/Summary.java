cceefd4688aaf213920c0fdb28a26d731259eecd
Rob Pellegrin <rob@localhost.localdomain>
2024-05-03 17:08:48 -0400

    Summary activity now pulls the data associated with currently signed in user and outputs it to the console

--- Project2/app/src/main/java/com/example/project2/Summary.java
+++ Project2/app/src/main/java/com/example/project2/Summary.java
@@ -2,6 +2,9 @@ package com.example.project2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.view.View;
-import android.widget.Button;
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class Summary extends AppCompatActivity {
     Button update;
+    FirebaseFirestore db;
+    FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +35,51 @@ public class Summary extends AppCompatActivity {
             return insets;
         });
         update = findViewById(R.id.Update);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        Log.d("Hi Mom", "This");
+
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Retrieve data from Firestore and output to console
+                retrieveAndOutputData();
+
                 // Launch CreateAccountActivity
                 Intent intent = new Intent(Summary.this, Update.class);
                 startActivity(intent);
             }
         });
+    }
 
+    private void retrieveAndOutputData() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+
+            // Query Firestore to retrieve data for the current user
+            db.collection("users")
+                    .document(userId)
+                    .collection("exerciseData")
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            // Iterate through the retrieved documents and output data to console
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String date = document.getString("date");
+                                String exerciseType = document.getString("exerciseType");
+                                String value = document.getString("value");
+
+                                // Output data to console
+                                Log.d("FirestoreData", "Date: " + date + ", Exercise Type: " + exerciseType + ", Value: " + value);
+                            }
+                        } else {
+                            Log.e("FirestoreData", "Error getting documents: ", task.getException());
+                        }
+                    });
+        } else {
+            // No user signed in
+            Log.e("FirestoreData", "No user signed in.");
+        }
     }
 }
