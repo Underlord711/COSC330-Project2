55808c49bfc7eb01667d67b5e9b01a40089017c1
Underlord711 <wchmar711@gmail.com>
2024-05-07 12:25:53 -0400

    Added Graph to admin View

--- Project2/app/src/main/java/com/example/project2/Admin.java
+++ Project2/app/src/main/java/com/example/project2/Admin.java
@@ -3,6 +3,7 @@ package com.example.project2;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,12 +18,25 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-public class Admin extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Admin extends AppCompatActivity {
+    private LineChart lineChart;
+    private List<String> xvalue;
     Button notifybtn;
     Button push;
     EditText editText;
@@ -36,6 +50,45 @@ public class Admin extends AppCompatActivity {
         editText = findViewById(R.id.writeText);
         push = findViewById(R.id.push);
         createNotificationChannel();
+        lineChart=findViewById(R.id.chart);
+        Description description= new Description();
+        description.setText("Students Record");
+        description.setPosition(150f,15f);
+        lineChart.setDescription(description);
+        lineChart.getAxisRight().setDrawLabels(false);
+        XAxis xAxis=lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xvalue= Arrays.asList("Nadon","Kamal","John");
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(xvalue));
+        xAxis.setLabelCount(3);
+        xAxis.setGranularity(1f);
+        YAxis yAxis=lineChart.getAxisLeft();
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(100f);
+        yAxis.setAxisLineWidth(2f);
+        yAxis.setAxisLineColor(Color.BLACK);
+        yAxis.setLabelCount(15);
+
+        List<Entry> entries1= new ArrayList<>();
+        entries1.add(new Entry(0,10f));
+        entries1.add(new Entry(1,10f));
+        entries1.add(new Entry(2,35f));
+        entries1.add(new Entry(3,45f));
+
+        List<Entry> entries2= new ArrayList<>();
+        entries2.add(new Entry(0,5f));
+        entries2.add(new Entry(1,15f));
+        entries2.add(new Entry(2,20f));
+        entries2.add(new Entry(3,30f));
+
+        LineDataSet dataSet1 = new LineDataSet(entries1, "Maths");
+        dataSet1.setColor(Color.BLUE);
+        LineDataSet dataSet2 = new LineDataSet(entries2, "Science");
+        dataSet2.setColor(Color.RED);
+
+        LineData lineData=new LineData(dataSet1,dataSet2);
+        lineChart.setData(lineData);
+        lineChart.invalidate();
 
         FirebaseMessaging.getInstance().subscribeToTopic("weather")
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -67,7 +120,7 @@ public class Admin extends AppCompatActivity {
                 String body = editText.getText().toString().trim();
                 editText.setVisibility(View.INVISIBLE);
                 editText.setEnabled(false);
-                sendNotification("HI MOM", body);
+                sendNotification("Wellness", body);
                 FirebaseMessaging.getInstance().getToken()
                         .addOnCompleteListener(new OnCompleteListener<String>() {
                             @Override
